c839169b58e886b88587b275d5375585428684b4
Brandon Coll <brandoncoll@brandons-mac.local>
2016-05-22 22:40:12 -0400

    Set up the storyboard and added tableView code.

--- BookShare/FirstViewController.swift
+++ BookShare/FirstViewController.swift
@@ -8,18 +8,45 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
-
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    // This array will need to be replaced with an API for pulling data from a server.
+    // Additional methods will be necessary to determine how much data should be pulled and what should be pulled.
+    var items: [String] = ["Testing", "This", "Table", "View"]
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // Counts the number of items to be displayed in the table. Returns this value as an Int.
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items.count
+    }
+    
+    // Creates each individual cell in the table.
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell:UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier("cell")! as UITableViewCell
+        
+        cell.textLabel?.text = self.items[indexPath.row]
+        
+        return cell
+    }
+    
+    // Handles selected cells. Currently needs to be implemented.
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        print("You selected cell #\(indexPath.row)!")
+    }
 
 
 }
-
